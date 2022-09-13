c9fef8f54db4a7b2703bc6c831d8ac8498d30029
EfeAdzhar <efeadzhar@gmail.com>
2022-09-13 23:26:21 +0200

    [HW-12][Commit-1]: Maded test for both classes. Not working properly

--- src/test/java/CommonPasswordCheckerTest.java
+++ src/test/java/CommonPasswordCheckerTest.java
@@ -1,3 +1,34 @@
-public class CommonPasswordCheckerTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class CommonPasswordCheckerTest {
+    CommonPasswordChecker commonPasswordChecker;
+
+    @BeforeEach
+    public void setUp() {
+        System.out.println("hello");
+        commonPasswordChecker = new CommonPasswordChecker();
+    }
+
+    @ParameterizedTest
+    @MethodSource("setCommonPasswordChecker")
+    public void testValid(String password) {
+        commonPasswordChecker.checkCommonPassword(password);
+    }
+
+
+    public static Set<Arguments> setCommonPasswordChecker() {
+         return Set.of(
+                 Arguments.of("hello"),
+                 Arguments.of(""),
+                 Arguments.of("what's up")
+         );
+    }
 }
--- src/test/java/PasswordValidatorTest.java
+++ src/test/java/PasswordValidatorTest.java
@@ -1,2 +1,38 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.Mock;
+
 public class PasswordValidatorTest {
+
+      PasswordValidator passwordValidator;
+
+    @Mock
+    private CommonPasswordChecker commonPasswordChecker;
+
+    @BeforeEach
+    public void setUp() {
+        passwordValidator = new PasswordValidator(commonPasswordChecker);
+    }
+
+    @Test
+    public void testToShort() {
+        String password = "h";
+        assertFalse(passwordValidator.checkPassword(password));
+    }
+
+    @Test
+    public void testToLong() {
+        String password = "1234567876543245676543234565432345643";
+        assertFalse(passwordValidator.checkPassword(password));
+    }
+
+    //NOT WORKING
+    @Test
+    public void testOk() {
+        String password = "1234567";
+        assertTrue(passwordValidator.checkPassword(password));
+    }
 }
